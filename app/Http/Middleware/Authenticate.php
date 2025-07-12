73871545a1198068e3036829898f9e3f4b768730
Burhan Mafazi <burhanburdev@gmail.com>
2025-07-12 23:47:43 +0700

    feat: middleware auth central

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,20 +10,47 @@ class Authenticate
 {
     public function handle(Request $request, Closure $next)
     {
-        $token = @$_COOKIE['central_access_token'];
+        $cookieName = 'central_access_token';
+        $token = @$_COOKIE[$cookieName];
         $headers = [
+            'x-app-id: ' . config('central.client_app_id'),
             'Authorization: Bearer ' . $token,
         ];
-        $url = config('endpoint.users.url') . '/api/me';
-        $response = getCurl($url, null, $headers);
+
+        // check auth user central
+        $url = config('central.api_url') . '/api/v1/client/session';
+        $response = postCurl($url, null, $headers);
+
+        if (
+            $response 
+            && $response->success ?? false
+            && $response->data ?? false
+            && $response->data->cookie ?? false
+        ) {
+            setcookie(
+                $response->data->cookie->name,
+                $response->data->cookie->value,
+                [
+                    'expires' => time() + $response->data->cookie->expire,
+                    'path' => $response->data->cookie->path,
+                    'secure' => $response->data->cookie->secure,
+                    'httponly' => $response->data->cookie->httpOnly,
+                    'samesite' => $response->data->cookie->sameSite
+                ]
+            );
+        }
+
+        // check auth user service
+        // $url = config('endpoint.users.url') . '/api/me';
+        // $response = getCurl($url, null, $headers);
 
         // Jika response tidak sukses, return unauthorized
         if (!$response || !isset($response->success) || $response->success !== true) {
             return Redirect::to(config('central.auth_url'));
         }
 
-        session(['username' => $response->data->username]);
-        session(['nama' => $response->data->nama]);
+        session(['username' => $response->data->user->username]);
+        session(['nama' => $response->data->user->full_name]);
 
         // Jika semua validasi berhasil, lanjutkan ke request berikutnya
         return $next($request);
--- config/central.php
+++ config/central.php
@@ -2,4 +2,6 @@
 
 return [
     'auth_url' => env('CENTRAL_AUTH_URL', 'https://centraldev.universitaspertamina.ac.id'),
+    'api_url' => env('CENTRAL_API_URL', 'http://127.0.0.1:8001'),
+    'client_app_id' => env('CENTRAL_APP_ID', null),
 ];
