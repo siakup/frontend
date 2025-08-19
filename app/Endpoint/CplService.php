cf2caa2c817c274c6154bd2489eccd8d44f7e1c1
Nath. <adriankalalo654@gmail.com>
2025-08-19 15:35:47 +0700

    cpl matakuliah get listing

---
+++ app/Endpoint/CplService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Endpoint;
+
+class CplService
+{
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Constructs the base URL for accessing the calendar API.
+     *
+     * @return string The base URL for the calendar API.
+     */
+
+    public function url()
+    {
+        return config('endpoint.lecture.url') . '/api/cpl';
+    }
+    public function cplUrl($id)
+    {
+        return $this->url() . '/' . $id;
+    }
+}
