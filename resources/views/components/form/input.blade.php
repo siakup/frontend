ef308a81979fcba920ed275673e0f266aff7d527
Notevenmore <hafizsecond635@gmail.com>
2025-11-07 14:55:16 +0700

    Fixing input components

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -78,7 +78,7 @@
 {{-- Input default --}}
 @else
   <div
-    class="p-4 sm:p-5 border-[1px] border-[#D9D9D9] text-sm w-full box-border mx-auto !px-3 rounded-lg leading-5 h-10 flex items-center read-only:bg-[#F5F5F5] {{ $disabled ? '!bg-[#F5F5F5] text-[#8C8C8C] cursor-not-allowed' : 'bg-white' }} {{ $inputClass }}"
+    class="p-4 sm:p-5 border-[1px] border-[#D9D9D9] text-sm w-full box-border mx-auto !px-3 rounded-lg leading-5 h-10 flex items-center {{ $disabled || $attributes->has('readonly') ? '!bg-[#F5F5F5] text-[#8C8C8C] cursor-not-allowed' : 'bg-white' }} {{ $inputClass }}"
     x-data="{
       removeButton: false,
       showRemoveIcon: {{ $showRemoveIcon ? 'true' : 'false' }},
@@ -101,7 +101,7 @@
     @if($iconUrl)
       <img 
         class="cursor-pointer" 
-        :class="{'hidden': !removeButton || !showRemoveIcon}"
+        x-bind:class="{'hidden': !removeButton || !showRemoveIcon}"
         src="{{ $iconUrl }}" 
         alt="" 
         x-on:click="if(showRemoveIcon) { value = ''; removeButton=false; }"
