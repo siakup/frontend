a5fd5128ef7ca13cf532d0e9610f21c85a37bbe3
adindaraisa <adindazhr11@gmail.com>
2025-11-26 15:49:00 +0700

    refactor: komponen (tooltip, dialog, badge)

--- resources/views/components/button/secondary.blade.php
+++ resources/views/components/button/secondary.blade.php
@@ -29,7 +29,7 @@
     @endif
     {{ 
       $attributes->merge([
-        'class' => "inline-flex w-fit w-max justify-center items-center gap-1 px-4 py-2 rounded-lg bg-white border border-[#E62129] text-[#E62129] hover:bg-[#FBDADB] active:bg-[#F7B6B8] cursor-pointer disabled:border-[#E8E8E8] disabled:text-[#8C8C8C] disabled:bg-white disabled:cursor-not-allowed {$class}"
+        'class' => "inline-flex w-fit w-max justify-center items-center gap-1 px-4 py-2 rounded-sm bg-white border border-[#E62129] text-[#E62129] hover:bg-[#FBDADB] active:bg-[#F7B6B8] cursor-pointer disabled:border-[#E8E8E8] disabled:text-[#8C8C8C] disabled:bg-white disabled:cursor-not-allowed {$class}"
       ]) 
     }}
   >
