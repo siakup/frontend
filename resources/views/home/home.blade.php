0bc9af136661e23a60c2096223d6bd2c78b1169c
Albert Lukas Talupan Pangaribuan <albert.ltp@universitaspertamina.ac.id>
2025-12-10 10:56:24 +0700

    [bug] fix home layout

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Beranda')
 
 @section('content')
-  <x-container.wrapper :rows="12" :padding="'p-0'">
+  <x-container.wrapper cols="1" padding="p-0">
 
-    <x-container.container :background="'transparent'" class="row-start-1 row-end-2">
+    <x-container.container background="transparent" align="start">
       <x-typography :variant="'body-large-semibold'">Beranda</x-typography>
     </x-container.container>
 
-    <x-container.container :background="'transparent'" class="row-start-2 row-end-13">
+    <x-container.container background="transparent">
       <x-container.container :background="'content-white'" :height="'maxContent'" :width="'full'" :padding="'p-4'">
         Ini beranda SIAKUP. Selamat menjelajah!
       </x-container.container>
