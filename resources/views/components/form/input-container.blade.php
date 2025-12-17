0f47017a4d1fd7b816f9911291dbefcfb4aee143
Albert Lukas Talupan Pangaribuan <albert.ltp@universitaspertamina.ac.id>
2025-12-17 14:07:37 +0700

    [refactor] layouts

--- resources/views/components/form/input-container.blade.php
+++ resources/views/components/form/input-container.blade.php
@@ -1,22 +1,21 @@
 @props(['containerClass' => '', 'labelClass' => '', 'inputClass' => '', 'labelWrap' => false])
 
-<x-container.wrapper :padding="'p-0'" :cols="9" :align="'center'" :justify="'center'" class="{{ $containerClass }}">
+<x-container.wrapper :padding="'p-0'" :cols="9" :align="'center'" :justify="'center'"
+    class="{{ $containerClass }}">
 
-  <x-container.container class="col-start-1 col-end-2">
-    <label 
-        {{ $attributes->merge([
-            'class' => "text-gray-800 text-sm font-semibold flex items-center " 
-                      . ($labelWrap ? '' : 'flex-shrink-0') 
-                      . " $labelClass"
-        ]) }}
-        for="{{ $attributes->get('for') }}"
-    >
-        {{ $label }}
-    </label>
-  </x-container.container>
+    <x-container.container class="col-start-1 col-end-2">
+        <label
+            {{ $attributes->merge([
+                'class' =>
+                    'text-gray-800 text-sm font-semibold flex items-center ' . ($labelWrap ? '' : 'flex-shrink-0') . " $labelClass",
+            ]) }}
+            for="{{ $attributes->get('for') }}">
+            {{ $label }}
+        </label>
+    </x-container.container>
 
-  <x-container.container :height="'maxContent'" class="col-start-3 col-end-10 items-center {{ $inputClass }}">
-      {{ $input }}
-  </x-container.container>
+    <x-container.container :height="'max'" class="col-start-3 col-end-10 items-center {{ $inputClass }}">
+        {{ $input }}
+    </x-container.container>
 
 </x-container.wrapper>
