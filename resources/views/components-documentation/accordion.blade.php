23724cae5ff3974ca39aad4073f32a675c3c84f9
irfanwk <irfan.wk.2705@gmail.com>
2025-12-21 11:26:18 +0700

    adding documentation accordion 2

--- resources/views/components-documentation/accordion.blade.php
+++ resources/views/components-documentation/accordion.blade.php
@@ -3,17 +3,21 @@
 @section('title', 'Accordion Documentation')
 
 @section('content')
+    {{-- Terdapat tiga parameter untuk accordion --}}
+    {{-- label: label dari accordion --}}
+    {{-- variant: variasi dari accordion (red-gradient dan white-background). Defaultnya adalah red-gradient --}}
+    {{-- isDefaultOpen: menentukan apakah accordion terbuka secara default atau tidak (true/false). Defaultnya adalah false--}}
 
     <x-container.container variant="content-wrapper" class="flex flex-col gap-5">
         
-        <x-typography variant="body-large-semibold">Penggunaan accordion pada element</x-typography>
+        <x-typography variant="body-large-semibold">Penggunaan accordion</x-typography>
 
         <x-container.container class="flex flex-col gap-5" borderRadius="rounded-lg">
 
             {{-- Red Gradient --}}
             <x-typography variant="body-medium-semibold">Red Gradient (Default)</x-typography>
             <div class="flex flex-col gap-3">
-                <x-accordion label="Accordion Default">
+                <x-accordion label="Accordion Red Gradient (Default)" variant="red-gradient">
                     <div class="p-4 bg-gray-50 text-gray-700">
                         Ini adalah konten untuk accordion dengan varian default (red-gradient).
                         Anda bisa memasukkan teks, gambar, atau komponen lain di sini.
