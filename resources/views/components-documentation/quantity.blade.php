9187a7ba1bf2b2db2cd1827dcc60554ff05cf75c
irfanwk <irfan.wk.2705@gmail.com>
2025-12-24 11:53:54 +0700

    FIX quantity form logic

--- resources/views/components-documentation/quantity.blade.php
+++ resources/views/components-documentation/quantity.blade.php
@@ -8,16 +8,42 @@
 
 @section('content')
 
-<x-container.container :variant="'content-wrapper'" class="!w-max !mx-0 !pb-10 !flex !flex-col !items-start !justify-start" x-data="{num: 0, num1: 0, num2: 0, num3: 0}">
-  <x-typography :variant="'body-small-bold'">Quantity Min: 0</x-typography>
-  <x-form.quantity x-model="num" :min="0" />
-  <x-typography :variant="'body-small-bold'">Quantity Max: 0</x-typography>
-  <x-form.quantity x-model="num1" :max="0" />
-  <x-typography :variant="'body-small-bold'">Quantity Min: 0, Max: 1</x-typography>
-  <x-form.quantity x-model="num2" :min="0" :max="1" />
-  <x-typography :variant="'body-small-bold'">Quantity Disabled</x-typography>
-  <x-form.quantity x-model="num3" :min="0" :max="1" :disabled="true" />
-</x-container>
+  <x-container.wrapper>
 
+    {{-- Title Row --}}
+    <x-container.container col="1">
+      <x-typography variant="body-large-semibold">Penggunaan Quantity</x-typography>
+    </x-container.container>
+
+    {{-- Content Row --}}
+    <x-container.container col="1">
+      <x-container.wrapper rows="4" class="gap-6">
+
+        {{-- variant quantity min 0 --}}
+        <x-container.container row="1" class="flex flex-col gap-3 w-fit">
+          <x-typography variant="body-small-bold">Quantity Min: 0</x-typography>
+          <x-form.quantity x-model=0 :min="0" />
+        </x-container.container>
+
+        {{-- variant quantity max 0 --}}
+        <x-container.container row="1" class="flex flex-col gap-3 w-fit">
+          <x-typography variant="body-small-bold">Quantity Max: 0</x-typography>
+          <x-form.quantity x-model=0 :max="0" />
+        </x-container.container>
+
+        {{-- variant quantity min 0 max 1 --}}
+        <x-container.container row="1" class="flex flex-col gap-3 w-fit">
+          <x-typography variant="body-small-bold">Quantity Min: 0, Max: 1</x-typography>
+          <x-form.quantity x-model=0 :min="0" :max="1" />
+        </x-container.container>
+
+        {{-- variant quantity disabled --}}
+        <x-container.container row="1" class="flex flex-col gap-3 w-fit">
+          <x-typography variant="body-small-bold">Quantity Disabled</x-typography>
+          <x-form.quantity x-model=0 :min="0" :max="1" :disabled="true" />
+        </x-container.container>
+      </x-container.wrapper>
+    </x-container.container>
+  </x-container.wrapper>
 
 @endsection
--- resources/views/components/form/quantity.blade.php
+++ resources/views/components/form/quantity.blade.php
@@ -3,12 +3,14 @@
   'max' => null,
   'disabled' => false
 ])
-<x-container.container 
+
+<x-container.wrapper 
   x-data="{
     value: 0,
     min: {{ json_encode($min) }},
     max: {{ json_encode($max) }},
     disabled: {{ json_encode($disabled) }},
+    holdInterval: null,
     checkIsMax() {
       if(!this.disabled) {
         if(this.max == null) return false
@@ -24,6 +26,12 @@
       return true
     },
     checkIsMinOrMax(value) {
+      value = Number(value);
+      // Jika value negatif, set ke 0 atau min value
+      if(value < 0) {
+        this.value = (this.min !== null && this.min >= 0) ? this.min : 0;
+        return;
+      }
       if(!this.disabled) {
         if((value <= this.max || this.max == null) && (value >= this.min || this.min == null)) this.value = value
         else {
@@ -32,46 +40,94 @@
             : ((value < this.min && this.min !== null) ? this.min : 0)
         }
       }
+    },
+    startHold(operation) {
+      if(this.disabled) return;
+      // First execution
+      if(operation === 'increment' && !this.checkIsMax()) {
+        this.value = Number(this.value) + 1;
+      } else if(operation === 'decrement' && !this.checkIsMin()) {
+        this.value = Number(this.value) - 1;
+      }
+      // Then start interval
+      let delay = 500; // Initial delay before continuous
+      this.holdInterval = setTimeout(() => {
+        this.holdInterval = setInterval(() => {
+          if(operation === 'increment' && !this.checkIsMax()) {
+            this.value = Number(this.value) + 1;
+          } else if(operation === 'decrement' && !this.checkIsMin()) {
+            this.value = Number(this.value) - 1;
+          } else {
+            this.stopHold();
+          }
+        }, 70); // Speed of continuous increment/decrement
+      }, delay);
+    },
+    stopHold() {
+      if(this.holdInterval) {
+        clearInterval(this.holdInterval);
+        clearTimeout(this.holdInterval);
+        this.holdInterval = null;
+      }
     }
   }"
-  class="w-max h-max flex flex-row justify-between items-center gap-3 p-2 rounded-sm border border-gray-400"
+  width="fit"
   x-modelable="value"
   x-model="{{$attributes->whereStartsWith('x-model')->first()}}"
 >
-  <div x-on:click="if(!checkIsMin()) value -= 1">
-    <template x-if="checkIsMin()">
-      <x-icon :name="'circle-remove/grey-16'" />
-    </template>
-    <template x-if="!checkIsMin()">
-      <x-icon :name="'circle-remove/red-16'" />
-    </template>
-  </div>
-  <style>
-    .no-spinners::-webkit-inner-spin-button,
-    .no-spinners::-webkit-outer-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-    }
-  </style>
-  <input 
-    type="number" 
-    class="no-spinners appearance-none focus:outline-none border-none focus:border-none text-center caption [-moz-appearance:textfield]" 
-    x-bind:class="{
-      'text-gray-800': !disabled,
-      'text-gray-500': disabled
-    }"
-    x-model="value"
-    x-bind:readonly="disabled"
-    x-bind:min="min"
-    x-bind:max="max"
-    x-on:input="checkIsMinOrMax($event.target.value)"
-  />
-  <div x-on:click="if(!checkIsMax()) value += 1">
-    <template x-if="checkIsMax()">
-      <x-icon :name="'circle-add/grey-16'" />
-    </template>
-    <template x-if="!checkIsMax()">
-      <x-icon :name="'circle-add/red-16'" />
-    </template>
-  </div>
-</x-container>
+  <x-container.container col="1" class="rounded-sm border border-gray-400">
+    <x-container.wrapper cols="3" gap="2" items="center" padding="p-2">
+      
+      {{-- Minus Button --}}
+      <x-container.container col="1" class="cursor-pointer flex items-center justify-center select-none" 
+        x-on:mousedown="startHold('decrement')"
+        x-on:mouseup="stopHold()"
+        x-on:mouseleave="stopHold()">
+        <template x-if="checkIsMin()">
+          <x-icon :name="'circle-remove/grey-16'" />
+        </template>
+        <template x-if="!checkIsMin()">
+          <x-icon :name="'circle-remove/red-16'" />
+        </template>
+      </x-container.container>
+
+      {{-- Input --}}
+      <x-container.container col="1" class="flex items-center justify-center">
+        <style>
+          .no-spinners::-webkit-inner-spin-button,
+          .no-spinners::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+          }
+        </style>
+        <input 
+          type="number" 
+          class="no-spinners appearance-none focus:outline-none border-none focus:border-none text-center caption [-moz-appearance:textfield] w-full p-0" 
+          x-bind:class="{
+            'text-gray-800': !disabled,
+            'text-gray-500': disabled
+          }"
+          style="background-color: transparent;"
+          x-model.number="value"
+          x-bind:readonly="disabled"
+          x-bind:min="min"
+          x-bind:max="max"
+          x-on:input="checkIsMinOrMax($event.target.value)"
+        />
+      </x-container.container>
+
+      {{-- Plus Button --}}
+      <x-container.container col="1" class="cursor-pointer flex items-center justify-center select-none" 
+        x-on:mousedown="startHold('increment')"
+        x-on:mouseup="stopHold()"
+        x-on:mouseleave="stopHold()">
+        <template x-if="checkIsMax()">
+          <x-icon :name="'circle-add/grey-16'" />
+        </template>
+        <template x-if="!checkIsMax()">
+          <x-icon :name="'circle-add/red-16'" />
+        </template>
+      </x-container.container>
+    </x-container.wrapper>
+  </x-container.container>
+</x-container.wrapper>
